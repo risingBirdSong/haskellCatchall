94bf66cc488659bafc0ed845f2a9851100a59de0
risingbirdsong <heregoingthere@outlook.com>
2020-10-15 21:57:49 -0700

    oh that is such a great example, really cool function :) give a tree a list of directions and find the element there

--- src/treeA.hs
+++ src/treeA.hs
@@ -28,3 +28,12 @@ changeToP :: Tree Char -> Tree Char
 changeToP (Node x l (Node y (Node _ m n) r)) = Node x l (Node y (Node 'P' m n) r) 
 changeToP' :: Tree Char -> Tree Char  
 changeToP' (Node x l (Node y (Node _ m n) r)) = Node x l (Node y (Node 'P' m n) r) 
+
+data Direction = L | R deriving (Show)  
+type Directions = [Direction] 
+
+elemAt :: Directions -> Tree a -> a  
+elemAt (L:ds) (Node _ l _) = elemAt ds l  
+elemAt (R:ds) (Node _ _ r) = elemAt ds r  
+elemAt [] (Node x _ _) = x  
+
