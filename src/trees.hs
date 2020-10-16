9eee1021a3e752a1d65cadc9a5069a1075ef6d91
risingbirdsong <heregoingthere@outlook.com>
2020-10-15 20:12:01 -0700

    fixed bug where i was recursing down the wrong path for contains depending on < or >

--- src/trees.hs
+++ src/trees.hs
@@ -4,10 +4,18 @@ empty :: Tree a -> Bool
 empty Nil = True
 empty _ = False
 
-insert Nil x = (Node (Nil) x (Nil))
+insert :: Ord a => Tree a -> a -> Tree a
+insert Nil x = Node Nil x Nil
 insert (Node l v r) x
   | v == x = (Node l v r)
   | v < x = (Node (insert l v) x r)
   | v > x = (Node l x (insert r v))
 
-testTree = foldl insert Nil [1,2,3,4]
+  
+contains Nil _ = False
+contains (Node l v r) x
+  | v == x = True
+  | x < v = contains l x
+  | x > v = contains r x
+
+testTree = foldl insert Nil  [1,2,3,4]
