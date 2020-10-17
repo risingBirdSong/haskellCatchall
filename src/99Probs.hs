9b099b88907803edf6b4f022786eeae59b0f1e4b
risingbirdsong <heregoingthere@outlook.com>
2020-10-16 17:21:52 -0700

    well cant say I fully understand it but folding with const id is interesting and solves the challenge of finding the last in the list

--- src/99Probs.hs
+++ src/99Probs.hs
@@ -11,3 +11,18 @@ sol1' x (y:ys) = sol1' y ys
 -- built in
 sol1a [] = Nothing
 sol1a xs = Just (last xs)
+
+-- Problem 2
+-- (*) Find the last but one element of a list.
+
+fld ls = foldr1 (const id) ls
+fld1 = foldl (const id) 0 [1,2,3]
+hm x  = (const id) x
+
+-- so 
+-- acc cur
+-- 0    1
+-- 0    2
+-- 0    3
+-- and (const id) will always select the 2nd of those
+-- the last of which is 3, so thats how the whole expression evaluates to 3
