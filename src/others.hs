0e44bf414a1dd2d565dce3f475907e77fa5d6ead
risingbirdsong <heregoingthere@outlook.com>
2020-10-21 16:11:33 -0700

    and another way to sort that is most efficient

--- src/others.hs
+++ src/others.hs
@@ -1,5 +1,9 @@
-import Data.List (sortBy)
+import Data.List (sortBy, sortOn)
 import Data.Ord (compare, comparing)
+
+rotate = drop <> take
+-- rotate 3 [1,2,3,4,5]
+-- [4,5,1,2,3]
 test_lsort =  ["abc","de","fgh","de","ijkl","mn","o"]
 
 -- https://stackoverflow.com/questions/57578855/what-is-the-best-way-to-realize-a-b-as-an-applicative-functor/57580348#57580348
@@ -7,4 +11,8 @@ test_lsort =  ["abc","de","fgh","de","ijkl","mn","o"]
 
 composedSort :: [String] -> [String]
 composedSort = sortBy $ (comparing length) <> compare
+
+-- in general, whenever the thing you're comparing is expensive to calculate, 
+-- sortOn makes sure its only evaluated once
+efficientSort xs = sortOn (\x -> (length x, x)) xs
 
