9b953aa13b3fd2ea86aede8e1c1a322f4a7b83ac
risingbirdsong <heregoingthere@outlook.com>
2020-10-23 10:41:43 -0700

    trying to understand this cbalTree, I replaced the char x in [Branch 'x' left right, with q and r and it looks like they are mostly zeroes and ones

--- src/99probs_trees.hs
+++ src/99probs_trees.hs
@@ -19,3 +19,12 @@ qrt = quotRem 5 5 -- (1,0)
 qrta = quotRem 7 5 -- (1,2)
 qrtb = quotRem 10 5 -- (2, 0)
 qrtc = quotRem 12 5 -- (2, 2)
+
+data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show, Eq)
+
+-- cbalTree :: Int -> [Tree Char]
+cbalTree 0 = [Empty]
+cbalTree n = let (q, r) = (n - 1) `quotRem` 2
+    in [Branch i left right | i     <- [q .. q + r],
+                                left  <- cbalTree i,
+                                right <- cbalTree (n - i - 1)]
