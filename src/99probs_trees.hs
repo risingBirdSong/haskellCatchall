1a9ab56ad4dbba8717d2d3720f80a715d8f17c67
risingbirdsong <heregoingthere@outlook.com>
2020-10-23 10:35:08 -0700

    very useful func quotRem that will help make balanced tree

--- src/99probs_trees.hs
+++ src/99probs_trees.hs
@@ -12,3 +12,10 @@
 -- Non-solution:
 
 -- Haskell's type system ensures that all terms of type Tree a are binary trees: it is just not possible to construct an invalid tree with this type. Hence, it is redundant to introduce a predicate to check this property: it would always return True.
+
+-- qoutRem
+-- (integer division, remainder)
+qrt = quotRem 5 5 -- (1,0)
+qrta = quotRem 7 5 -- (1,2)
+qrtb = quotRem 10 5 -- (2, 0)
+qrtc = quotRem 12 5 -- (2, 2)
