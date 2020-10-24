6db69294cf5fcd5c31f10331a67adbf613d79c90
risingbirdsong <heregoingthere@outlook.com>
2020-10-23 19:36:10 -0700

    the rest of my learning from the vid, with an awesome fibonacci seq using list comp

--- src/derekbanas.hs
+++ src/derekbanas.hs
@@ -40,3 +40,48 @@ listTimes3 = [x * 3 | x <- [1..20], x*3 <= 50]
  
 -- Return all values that are divisible by 13 and 9
 divisBy9N13 = [x | x <- [1..500], x `mod` 13 == 0, x `mod` 9 == 0]
+
+-- cool alt to standard factorial
+productFact n = product [1..n]
+
+-- example of as pattern
+-- We can also get values with an As pattern
+getFirstItem :: String -> String
+getFirstItem [] = "Empty String"
+getFirstItem all@(x:xs) = "The first letter in " ++ all ++ " is "
+  ++ [x]
+  
+
+  -- nice example
+-- Check if strings are equal with recursion
+areStringsEq :: [Char] -> [Char] -> Bool
+areStringsEq [] [] = True
+areStringsEq (x:xs) (y:ys) = x == y && areStringsEq xs ys
+areStringsEq _ _ = False
+
+-- chainging recursive &&s in the above func areStringsEq
+chainedAnds = True && True && True && True --True
+
+-- ---------- EXAMPLE : FIBONACCI SEQUENCE ----------
+ 
+-- Calculate the Fibonacci Sequence
+-- 1, 1, 2, 3, 5, 8, ...
+ 
+-- 1 : 1 : says to add 2 1s to the beginning of a list
+-- | for every (a, b) add them
+-- <- stores a 2 value tuple in a and b
+-- tail : get all list items minus the first
+-- zip creates pairs using the contents from 2 lists being the lists fib and the 
+-- list (tail fib)
+ 
+fib = 1 : 1 : [a + b | (a, b) <- zip fib (tail fib) ]
+ 
+-- First time through fib = 1 and (tail fib) = 1
+-- The list is now [1, 1, 2] because a: 1 + b: 1 = 2
+ 
+-- The second time through fib = 1 and (tail fib) = 2
+-- The list is now [1, 1, 2, 3] because a: 1 + b: 2 = 3
+ 
+fib300 = fib !! 300 -- Gets the value stored in index 300 of the list
+ 
+-- take 20 fib returns the first 20 Fibonacci numbers
---
+++ src/pairprogramming.hs
@@ -0,0 +1,5 @@
+-- reverse 
+
+-- https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/haskell
+
+-- factorial
