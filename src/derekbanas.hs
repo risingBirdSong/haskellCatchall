65323bc52b1671be10a26aa047a3ebad7c5323ed
risingbirdsong <heregoingthere@outlook.com>
2020-10-23 17:12:49 -0700

    derek banas primitive data

---
+++ src/derekbanas.hs
@@ -0,0 +1,7 @@
+import System.IO
+import Data.List
+boundedInt = maxBound :: Int -- 9223372036854775807
+
+-- Integer is unbounded whole number can be as big as memory can hold, huge number
+
+-- prefer Doubles over floats, up to 11 points decimal precision
