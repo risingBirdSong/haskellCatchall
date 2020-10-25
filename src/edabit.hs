28c617b912418fc25ae7b469945b1280a9bae549
risingbirdsong <heregoingthere@outlook.com>
2020-10-24 17:59:55 -0700

    solved Add up the Numbers from a Single Number

--- src/edabit.hs
+++ src/edabit.hs
@@ -1,6 +1,12 @@
 import System.IO
 import Data.List
 
+matchHouses :: (Num p, Ord p) => p -> p
 matchHouses x 
   | x == 1 = 6
   | x > 1 = 6 + ((x-1) * 5)
+
+
+  -- Add up the Numbers from a Single Number
+
+addUp x = sum [1..x]
