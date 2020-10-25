431a428915226244581efa45cb6b3b95d234b34c
risingbirdsong <heregoingthere@outlook.com>
2020-10-24 18:27:32 -0700

    solved the larger func challenge in haskell, interesting simple problem

--- src/edabit.hs
+++ src/edabit.hs
@@ -8,5 +8,22 @@ matchHouses x
 
 
   -- Add up the Numbers from a Single Number
-
+  -- https://edabit.com/challenge/4gzDuDkompAqujpRi
 addUp x = sum [1..x]
+
+-- Which Function Returns the Larger Number?
+-- https://edabit.com/challenge/o7TwicAHWuMkjbDqQ
+
+
+rtrn5 :: () -> Integer
+rtrn5 = (\_ -> 5)
+rtrn6 :: () -> Integer
+rtrn6 = (\_ -> 6)
+
+
+largerFun x y = max x y
+
+lf :: (() -> Integer) -> (() -> Integer) -> Char
+lf x y 
+  | x() > y() = 'f'
+  | otherwise = 'g'
