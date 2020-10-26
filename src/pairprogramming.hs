aafcb6668ca5389bf3def1e5f1746e491f437533
risingbirdsong <heregoingthere@outlook.com>
2020-10-26 10:18:59 -0700

    went over baisc algos with Vaibhov, we reimplemented reverse together

--- src/pairprogramming.hs
+++ src/pairprogramming.hs
@@ -3,7 +3,8 @@
 -- https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/haskell
 
 -- factorial
-
+import Data.List
+import Debug.Trace
 
 myLast_ :: [p] -> p
 myLast_ [x] = x
@@ -37,3 +38,37 @@ rvrs [] = [] -- end case , base case, exit condition
 rvrs (start:rest) = rvrs (rest) ++ [start] -- doing magic, doing the work
 
 ttt = floor 5.5 
+
+-- Input: nums = [1,2,3,4]
+-- Output: [1,3,6,10]
+-- Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+runningSum lst = runningSum' lst 0
+runningSum' :: Num a => [a] -> a -> [a]
+runningSum' [] sum = []
+runningSum' (x:xs) sum = (sum + x) : runningSum' (xs) (sum + x)
+
+
+-- https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+-- [2,3,5,1,3], extraCandies = 3
+  
+original = [2,3,5,1,3]
+
+
+-- findMax :: [a] -> a
+findMax lst = last (sort lst)
+
+giveCandy lst extra = map (+extra) lst
+
+greater lst max = map (\x -> if x >= max then True else False) lst
+
+solve lst extra  = greater (distr extra) (max)
+    where max = findMax lst
+          distr = giveCandy lst
+
+
+-- tester lst = map 
+
+rev [] = [] 
+rev (start:rest) =  (rev rest) ++ [start] 
