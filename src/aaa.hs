b9e8dccd10697f345358a53aa30683d990e2ae56
risingbirdsong <heregoingthere@outlook.com>
2020-10-31 19:00:18 -0700

    solved another one with Bernie and Javi, how many children are working at the same hour

--- src/aaa.hs
+++ src/aaa.hs
@@ -7,3 +7,30 @@ solve mtx = length (concat (map (filternegs) mtx))
 
 practice =  filter (>5) [1,2,3,4,5,6,7,8]
 
+
+-- Input: [[1,1,0],[1,0,1],[0,0,0]]
+-- Output: [[1,0,0],[0,1,0],[1,1,1]]
+
+rrvssub mtrx = map (map invert) (map (reverse) mtrx)
+
+invert num 
+  | (num == 0) = 1
+  | otherwise = 0
+
+oneszeros = [[1,1,0],[1,0,1],[0,0,0]]
+
+
+both lst = map (invert) lst
+
+-- 1450. Number of Students Doing Homework at a Given Time
+
+startTime = [1,2,3,3]
+endTime = [3,2,7,8] 
+queryTime = 4
+
+qt (str) (end) (q) = length (concat (qt' (str) (end) (q) (0)))
+qt' [] _ _ _  = []
+qt' _ [] _ _ = []
+qt' (s:xs) (e:ys) q acc 
+  | ((e > q) && (s < q)) = [acc+1] : qt' (xs) (ys) q (acc+1)
+  | otherwise = qt' (xs) (ys) q  (acc)
