924bc53459ff5dd8aa80031194af17d32b2bafea
risingbirdsong <heregoingthere@outlook.com>
2020-11-03 10:49:47 -0800

    great example of inserting into trees with left and right folds, look at how the right fold uses flip to match the fold type, and how the list is reversed first since fold starts from the right

---
+++ src/bst_a.hs
@@ -0,0 +1,19 @@
+data Tree a = Node (Tree a) a (Tree a) | Nil deriving (Show, Eq, Ord)
+
+nsrt Nil v = Node Nil v Nil   
+nsrt (Node l a r) v
+  | (v == a) = Node l v r
+  | (v < a) = Node (nsrt l v) a r
+  | (v > a) = Node l a (nsrt r v)
+
+-- contains Nil v = False
+-- contains (Node l a r) v
+--   | v 
+
+-- foldl :: Foldable t => (b -> a -> b) -> b -> t a -> b
+fld_a = foldl (nsrt) Nil [5,4,6,3,7,2,8]
+--  (Node (Node (Node Nil 2 Nil) 3 Nil) 4 Nil) 5 (Node Nil 6 (Node Nil 7 (Node Nil 8 Nil)))
+
+-- foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
+fld_b =  foldr (flip nsrt) Nil (reverse [5,4,6,3,7,2,8])
+--  (Node (Node (Node Nil 2 Nil) 3 Nil) 4 Nil) 5 (Node Nil 6 (Node Nil 7 (Node Nil 8 Nil)))
