14f2307067fa24f0669720ff750780edb5b0050b
risingbirdsong <heregoingthere@outlook.com>
2020-11-02 23:10:37 -0800

    great solutions from community

--- src/paulf.hs
+++ src/paulf.hs
@@ -18,5 +18,24 @@ countDrop' cmp (x:xs) cnt
   | (x >= cmp) = countDrop' cmp (xs) (cnt + 1)
   | otherwise = cnt
 
+solve lst = solve' lst (sortBy (flip compare) lst)
+solve' _ [] = []
+solve' [] _ = []
+solve' (l:ls) (s:st) 
+  | (s == l) = [s] : solve' (ls) (st)
+  | (s >= l) = [s] : solve' (drop (countDrop l (s:st)) (l:ls)) (drop (countDrop l (s:st)) (s:st))
+  | otherwise = [s] : solve' (ls) (s:st)
+
 replace [x] = [-1]
 replace (l:lst) = (maximum lst) : replace (lst)
+
+
+replace' [_] = [-1]
+replace' (_ : x : xs) = let (y:ys) = replace' (x:xs) in max x y : y : ys
+
+replace'' [_] = [-1]
+replace'' (_ : xs) = let ys = replace'' xs in max (head xs) (head ys) : ys
+
+-- it's scanr1 max
+-- just tail it and append -1
+brilliantreplace = tail . scanr  max (-1) 
