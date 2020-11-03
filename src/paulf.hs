e65b6672c1c81f634f7e94e863514031d4167f9a
risingbirdsong <heregoingthere@outlook.com>
2020-11-02 21:33:04 -0800

    count how many we will want to drop later... this is helper function

--- src/paulf.hs
+++ src/paulf.hs
@@ -7,10 +7,16 @@
 import Data.List
 
 input = [17,18,5,4,6,1]
--- srted =
+--srted=[18,17,6,5,4,1]
  
 asc lst = sortBy (compare) lst
-desc lst =sortBy (flip compare) lst 
+desc lst = sortBy (flip compare) lst 
+
+countDrop cmp lst = countDrop' cmp lst 0 
+countDrop' cmp [] cnt = cnt
+countDrop' cmp (x:xs) cnt
+  | (x >= cmp) = countDrop' cmp (xs) (cnt + 1)
+  | otherwise = cnt
 
 replace [x] = [-1]
 replace (l:lst) = (maximum lst) : replace (lst)
