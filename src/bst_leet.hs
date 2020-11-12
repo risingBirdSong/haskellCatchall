72ea9c38e545ca8e54459cc1f583e98784a7926d
risingbirdsong <heregoingthere@outlook.com>
2020-11-12 14:39:07 -0800

    after some over thinking , misunderstanind how simple the problem is, I arrived at the answer for leet challenge 700

--- src/bst_leet.hs
+++ src/bst_leet.hs
@@ -1,27 +1,36 @@
-data Tree v = No | Nodey (Tree v) v (Tree v) deriving (Show, Eq, Ord)
 
-nsrt No v = Nodey No v No 
+data Tree v = Nil | Nodey (Tree v) v (Tree v) deriving (Show, Eq, Ord)
+
+nsrt Nil v = Nodey Nil v Nil 
 nsrt (Nodey l v r) n 
   | n == v = (Nodey l v r)
   | (n < v) = Nodey (nsrt l n) v r
   | (n > v) = Nodey l v (nsrt r n)  
 
-testT = foldl nsrt (No) [5,4,6,3,7,2,8]
+testT = foldl nsrt (Nil) [5,4,6,3,7,2,8]
+trTest = foldl nsrt (Nil) [4,2,7,1,3]
 
-contains No qry = False
+contains Nil qry = False
 contains (Nodey l v r) qry
   | (v == qry) = True
   | (qry < v) = contains l qry 
   | otherwise = contains r qry
 
-treeMap No _ = No
+treeMap Nil _ = Nil
 treeMap (Nodey l v r) fn = Nodey (treeMap l fn) (fn v) (treeMap r fn)
 
 -- todo learn how to do tree fold, attempted at it went nowhere
 
-rmv No _ = No
+rmv Nil _ = Nil
 rmv (Nodey l v r) delV
-  | (v == delV) = No
+  | (v == delV) = Nil
   | (delV < v) = Nodey (rmv l delV) v r
   | otherwise = Nodey l v (rmv r delV)
 
+-- 700. Search in a Binary Search Tree
+
+subT Nil _  = Nil
+subT (Nodey l v r) qry 
+  | qry == v = Nodey l v r
+  | (qry < v) = subT l qry
+  | (qry > v) = subT r qry 
