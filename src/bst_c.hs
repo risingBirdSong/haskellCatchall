6e5c799e27b7b7dd61d9f2ee2cec13efa1f08ebe
risingbirdsong <heregoingthere@outlook.com>
2020-11-12 13:21:20 -0800

    working with trees is getting easier, faster and more understood

---
+++ src/bst_c.hs
@@ -0,0 +1,18 @@
+data Tree v = No | Nodey (Tree v) v (Tree v) deriving (Show, Eq, Ord)
+
+nsrt No v = Nodey No v No 
+nsrt (Nodey l v r) n 
+  | n == v = (Nodey l v r)
+  | (n < v) = Nodey (nsrt l n) v r
+  | (n > v) = Nodey l v (nsrt r n)  
+
+testT = foldl nsrt (No) [5,4,6,3,7,2,8]
+
+contains No qry = False
+contains (Nodey l v r) qry
+  | (v == qry) = True
+  | (qry < v) = contains l qry 
+  | otherwise = contains r qry
+
+treeMap No _ = No
+treeMap (Nodey l v r) fn = Nodey (treeMap l fn) (fn v) (treeMap r fn)
