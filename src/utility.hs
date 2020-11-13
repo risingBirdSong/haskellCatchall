515607f6febec9f1cfe0aad497fe2065e4a889ac
risingbirdsong <heregoingthere@outlook.com>
2020-11-13 13:23:43 -0800

    wooohoo this one made me proud, likely not a great solution, but stretch me and it was a great learning process!!! doing this leet the hard way, by actually handling each sub array, rather than just reverse the arr and compare... for the learning and challenge

--- src/utility.hs
+++ src/utility.hs
@@ -1,18 +1,25 @@
 import Data.List
 takeUntil p = foldr (\x ys -> x : if p x then [] else ys) []
 
+dropUntil _ [] = []
 dropUntil trg (l:ls)
   | (trg == l) = ls 
   | otherwise = dropUntil trg ls 
 -- Input: target = [1,2,3,4], arr = [2,4,1,3]
 
-start = [2,4,1,3]
+start = [2,4,1,5]
 a = [1,4,2,3]
+b = [1,2,4,3]
 -- sub :: (Foldable t, Eq a) => a -> t a -> [a]
 sub trg ls drp = reverse $ drop drp $ takeUntil (== trg) ls
 worker trg ls drp = take drp ls ++ (sub trg ls drp) ++ dropUntil trg ls 
 
-grouper t ls drp =  reverse (sub t ls drp)
+solver trgArr arr = solver' trgArr trgArr arr 0
+solver' [] trgArr arr _ = False
+solver' (r:rcs) (trgArr) arr cnt
+  | (trgArr) == arr = True
+  | otherwise = solver' rcs (trgArr) (worker r arr cnt) (cnt + 1)
+
 
 -- ans [] arr = ori == arr
 -- ans (g:goal) arr = ans goal (:) 
