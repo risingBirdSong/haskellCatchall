f678dcd8809ee5a71024a6f7bff926d9ff78a1c8
risingbirdsong <heregoingthere@outlook.com>
2020-11-19 20:41:14 -0800

    implemented to num from digits

--- src/codewars.hs
+++ src/codewars.hs
@@ -4,11 +4,14 @@ sortNumbers [] = Nothing
 sortNumbers xs =  Just (sort xs)
 
 -- automorphic :: Integer -> String
--- automorphic num = 
-
+-- automorphic num = numList
+--           where digits = convertBtr' num 
+--                 lstw = drop ((length lstw)-2) lstw
 convertSlow 0 = []
 convertSlow num =  convertSlow (num `div` 10) ++ [snd (num `divMod` 10)]
 
 convertBtr num = reverse (convertBtr' num)
 convertBtr' 0 = []
 convertBtr' num = snd (num `divMod` 10) : convertBtr' (num `div` 10)
+
+toNum' lst = fst $ foldr (\x (acc,place) -> (acc + (place * x) , place * 10) ) (0,1) lst
