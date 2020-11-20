d54bdcfd7d65135ad6633130f48692e6d312541e
risingbirdsong <heregoingthere@outlook.com>
2020-11-19 21:21:33 -0800

    wooh lots of steps and learning, made some mistakes but overcame them. ++

--- src/codewars.hs
+++ src/codewars.hs
@@ -3,10 +3,18 @@ sortNumbers :: [Int] -> Maybe [Int]
 sortNumbers [] = Nothing
 sortNumbers xs =  Just (sort xs)
 
--- automorphic :: Integer -> String
--- automorphic num = numList
---           where digits = convertBtr' num 
---                 lstw = drop ((length lstw)-2) lstw
+automorphic :: Integer -> String
+automorphic num 
+  | amph num = "Automorphic"
+  | otherwise = "Not!!"
+amph num = num == end
+          where end = digitsToEnd num
+
+getend digits n = toNum (drop ((length digits) - (length (convertBtr n))) digits) 
+sqrThenLst n = convertBtr (n*n) 
+
+digitsToEnd num = getend (sqrThenLst num) num
+
 convertSlow 0 = []
 convertSlow num =  convertSlow (num `div` 10) ++ [snd (num `divMod` 10)]
 
@@ -14,4 +22,6 @@ convertBtr num = reverse (convertBtr' num)
 convertBtr' 0 = []
 convertBtr' num = snd (num `divMod` 10) : convertBtr' (num `div` 10)
 
-toNum' lst = fst $ foldr (\x (acc,place) -> (acc + (place * x) , place * 10) ) (0,1) lst
+toNum lst = fst $ foldr (\x (acc,place) -> (acc + (place * x) , place * 10) ) (0,1) lst
+
+sqr n = n * n 
