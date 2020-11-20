d6fe281830417d51c58ba18bae25bcea81c5d803
risingbirdsong <heregoingthere@outlook.com>
2020-11-19 21:23:50 -0800

    the first 3 functions to my little helper library, 3 useful funcs that can be used again and again

---
+++ src/mylibrary.hs
@@ -0,0 +1,12 @@
+
+convertBtr num = reverse (convertBtr' num)
+convertBtr' 0 = []
+convertBtr' num = snd (num `divMod` 10) : convertBtr' (num `div` 10)
+
+toNum lst = fst $ foldr (\x (acc,place) -> (acc + (place * x) , place * 10) ) (0,1) lst
+
+filterTwo f [] = [] 
+filterTwo f [x] = [] 
+filterTwo f (x:y:ls)
+  | (f x y) = x : y : filterTwo f (y:ls)
+  | otherwise = filterTwo f (y:ls)
