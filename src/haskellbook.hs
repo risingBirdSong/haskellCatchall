4e26fba0b123ecb829cac2588c97f8b85372bac3
risingbirdsong <heregoingthere@outlook.com>
2020-11-26 12:44:46 -0800

    simple exercises from the book

--- src/haskellbook.hs
+++ src/haskellbook.hs
@@ -14,3 +14,22 @@ aab = let x = 222; y = 333 in x * y -- 73926
 aac = x * y where x = 67; y = 91 -- 6097
 
 aad = x + y where x = 213; y = 4234 -- 4447
+
+myGreeting :: String
+myGreeting = (++) "hello" " world!"
+
+main :: IO ()
+main = do
+putStrLn myGreeting
+putStrLn secondGreeting
+  where secondGreeting = "hi again"
+
+
+aae =  'c' : "hris"
+aaf = 'P' : ""
+
+
+crry = "Curry is awesome"
+aag = take 1 $ drop 4 crry -- "y"
+
+takerdroper t d s= take t $ drop d s 
