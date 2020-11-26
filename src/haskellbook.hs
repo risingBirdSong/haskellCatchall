2194966ce3e0097cdd0514e60de1458280bb2740
risingbirdsong <heregoingthere@outlook.com>
2020-11-25 21:48:54 -0800

    more simple differences between let and where

--- src/haskellbook.hs
+++ src/haskellbook.hs
@@ -5,3 +5,12 @@ plusTwowhere n = print $ f n
 
 plusTwolet n = let f n = 2 + n  
                 in print $ f n 
+
+
+aaa = let x = 333; y = 666 in x + y -- 999
+aab :: Integer
+aab = let x = 222; y = 333 in x * y -- 73926
+
+aac = x * y where x = 67; y = 91 -- 6097
+
+aad = x + y where x = 213; y = 4234 -- 4447
