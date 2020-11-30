f37848edee2b41506f3ae332a18bc16c2e19c843
risingbirdsong <heregoingthere@outlook.com>
2020-11-30 08:40:44 -0800

    simple showing, it can and should be derived, but doing it for the practice

--- src/datadates.hs
+++ src/datadates.hs
@@ -1,4 +1,12 @@
-data DayOfWeek = Mon | Tue | Weds | Thu | Fri | Sat | Sun deriving (Show)
+data DayOfWeek = Mon | Tue | Weds | Thu | Fri | Sat | Sun
+instance Show DayOfWeek where 
+  show Mon = "monday"
+  show Tue = "tuesday"
+  show Weds = "wednesday"
+  show Thu = "thursday"
+  show Fri = "friday"
+  show Sat = "saturday"
+  show Sun = "sunday"
 
 data Date = Date DayOfWeek Int
 
