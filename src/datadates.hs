3cfa1f089f2df894f2cde1bee67634fbf3c8d241
risingbirdsong <heregoingthere@outlook.com>
2020-11-30 08:47:43 -0800

    hmm, I got that wrong previously, Haskell can derive Eq for the Date

--- src/datadates.hs
+++ src/datadates.hs
@@ -1,26 +1,13 @@
-data DayOfWeek = Mon | Tue | Weds | Thu | Fri | Sat | Sun
-instance Show DayOfWeek where 
-  show Mon = "monday"
-  show Tue = "tuesday"
-  show Weds = "wednesday"
-  show Thu = "thursday"
-  show Fri = "friday"
-  show Sat = "saturday"
-  show Sun = "sunday"
+data DayOfWeek = Mon | Tue | Weds | Thu | Fri | Sat | Sun deriving (Show , Eq)
 
-data Date = Date DayOfWeek Int
 
-instance Eq DayOfWeek where 
-  Mon == Mon = True 
-  Tue == Tue = True 
-  Weds == Weds = True 
-  Thu == Thu = True 
-  Fri == Fri = True 
-  Sat == Sat = True 
-  Sun == Sun = True
-  (==) _  _ = False
+data Date = Date DayOfWeek Int deriving (Show, Eq)
 
-instance Eq Date where 
-  (==) (Date weekDay dayNum)
-       (Date weekDay' dayNum')
-       = weekDay == weekDay' && dayNum == dayNum'
+day1 = Date Mon 1 
+dayOne = Date Mon 1
+day2 = Date Tue 2
+
+-- instance Eq Date where 
+--   (==) (Date weekDay dayNum)
+--        (Date weekDay' dayNum')
+--        = weekDay == weekDay' && dayNum == dayNum'
