9c1903930f6a94192173d57c29c1ec9914bf0337
risingbirdsong <heregoingthere@outlook.com>
2020-12-02 10:47:04 -0800

    yay! solved the first part of advent of code day 2

--- adventofcode/day2.hs
+++ adventofcode/day2.hs
@@ -1,29 +1,21 @@
 import Data.List.Split
-a = splitOn "," "a,bb,ccc,ddd"
-b = splitOn " " "1-7 q: qqqqxvqrkbqqztlqlzq"
-
-str = "1-7 q: qqqqxvqrkbqqztlqlzq"
-
-clean str =  words str
-cleaned = clean "1-7 q: qqqqxvqrkbqqztlqlzq"
 
 toNum :: [String] -> [Int]
 toNum = map read 
 
 bounds str = toNum $ splitOn "-" $ (!!) (clean str) 0
 ltr str = head $ take 1 $ (!!) (clean str) 1
-
 psswrd str = (!!) (clean str) 2
 
-password str = psswrd str
-flt letter str = filter (==letter) (password str )
-
-solveOne str = do 
-   let bnds = bounds str 
-   let letter = ltr str 
-   let password = psswrd str 
-   let count = length $ filter (==letter) password 
-   let solve = count >= (head bnds) && count <= (last bnds)
-   print solve
-   return ()
-
+solveOne str = solve
+  where bnds = bounds str 
+        letter = ltr str 
+        password = psswrd str 
+        count = length $ filter (==letter) password 
+        solve = count >= (head bnds) && count <= (last bnds)
+ 
+main = do 
+  listof <- lines <$> readFile "adventofcode//day2data.txt"
+  let passwordCount = length $ filter (solveOne) listof
+  print passwordCount
+  return ()
