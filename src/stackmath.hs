aaaeff3365becc249eb52d8c2a75efa8d8c7dc18
risingbirdsong <heregoingthere@outlook.com>
2020-12-06 15:00:42 -0800

    wanst sure that would work, but glad it did. finding the single max, and then keeping all the ones that equal the max, in a where binding

--- src/stackmath.hs
+++ src/stackmath.hs
@@ -37,20 +37,24 @@ findReducerB vals = reverse $ go vals []
   where go [x] acc = acc 
         go [] acc = acc
         go (a:b:ls) acc 
-          | a > (Just 1) && b > (Just 1) = go (b:ls) (Mul:acc)
+          | a > (Just 1) && b > (Just 1) = go ((multMybe a b):ls) (Mul:acc)
           | a == (Just 0) =  go (b:ls) (Pop:acc)
-          | a == (Just 1) || b == (Just 1) = go (b:ls) (Add:acc)
-          | a >= (Just 0) && b < (Just 0) = go (b:ls) (Sub:acc)
-          | a < (Just 0) && a >= b = go (b:ls) (Sub:acc)
+          | a == (Just 1) || b == (Just 1) = go (addMyb a b:ls) (Add:acc)
+          | a >= (Just 0) && b < (Just 0) = go (subMyb a b:ls) (Sub:acc)
+          | a < (Just 0) && a >= b = go (subMyb a b :ls) (Sub:acc)
           | a < (Just 0) && a < b = go (b:ls) (Pop:acc)
 -- -5 -1 -> pop, because its greater than subtraction
 -- -1 -4 -> sub, because its greater than two pops
 
 multMybe a b = ((*) <$> a <*> b )
-addMyb :: (Applicative f, Num b) => f b -> f b -> f b
 addMyb a b = ((+) <$> a <*> b)
 subMyb a b = ((-) <$> a <*> b)
 
 example1 = [Just 0, Just 1, Just 4, Just 5]
 example2 = [Just 10, Just (-1), Just 4, Just 5]
-ins1 = [Add,Add,Mul]
+instructions1 = [Add,Add,Mul]
+
+reducerSolve stack = allTheMaxes
+    where transformed = map (evalInst stack)  [findReducerA stack, findReducerB stack]
+          singleMax = maximum transformed
+          allTheMaxes = filter (==singleMax) transformed
