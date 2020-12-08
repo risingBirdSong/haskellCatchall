95f570e87c8e8ab124b3a7a609d90e2fc38e8b1a
risingbirdsong <heregoingthere@outlook.com>
2020-12-07 18:51:52 -0800

    hurray solved yourOrderPlease, it was a real fun one :)

--- src/codewars.hs
+++ src/codewars.hs
@@ -318,3 +318,13 @@ deleteAll dlt (v:vs)
   | otherwise = v : deleteAll dlt (vs)
 
 difference_a as bs = filter (`notElem` bs) as
+
+-- yourOrderPlease :: String -> String
+-- yourOrderPlease = 
+
+-- "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+-- "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+-- 
+
+yourOrderFirstStep ls = map (\x -> (find isDigit x, x)) $ words ls
+yourOrderPlease ls = unwords $ map (snd) $ sort $ map (\x -> (find isDigit x, x)) $ words ls
