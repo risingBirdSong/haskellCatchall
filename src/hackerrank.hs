95ef24ea139db03bcc25c3ce1e6385ad8a57b6d9
risingbirdsong <heregoingthere@outlook.com>
2020-12-19 14:48:18 -0800

    rewrote the main io portion from the boilerplate

--- src/hackerrank.hs
+++ src/hackerrank.hs
@@ -1,4 +1,4 @@
-import qualified Data.List
+import Data.List
 
 alice = [1,2,3]
 bob = [3,2,1]
@@ -7,6 +7,17 @@ tripletCompare (x,y) (a,b)
   | x < y = (a, succ b)
   | otherwise = (a,b)
 
--- compareTriplets a b = pairToList $ tripletCompare (Data.List.zip a b) (0,0)
 compareTriplets a b = pairToList $ foldr tripletCompare (0,0) $ zip a b 
 pairToList (a,b) = [a,b]
+
+main :: IO()
+main = do 
+  aTemp <- getLine 
+  let a = map (read :: String -> Int) $ words aTemp
+  bTemp <- getLine  
+  let b = map (read :: String -> Int) $ words bTemp
+  let ans = compareTriplets a b
+  print ans 
+  let result = intercalate " " $ map show ans
+  putStrLn result
+  return ()
