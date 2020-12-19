308703fc6ce6a29f5b3918280a99f0f91bc42f7b
risingbirdsong <heregoingthere@outlook.com>
2020-12-19 14:27:57 -0800

    solved again compare triplets, much cleaner using fold

--- src/hackerrank.hs
+++ src/hackerrank.hs
@@ -2,10 +2,11 @@ import qualified Data.List
 
 alice = [1,2,3]
 bob = [3,2,1]
-tripletCompare [] acc = acc 
-tripletCompare ((x,y):xs) (a,b)
-  | x > y = tripletCompare xs (succ a, b)
-  | x < y = tripletCompare xs (a, succ b) 
-  | x == y = tripletCompare xs (a,b)
-compareTriplets a b = pairToList $ tripletCompare (Data.List.zip a b) (0,0)
+tripletCompare (x,y) (a,b)
+  | x > y = (succ a, b)
+  | x < y = (a, succ b)
+  | otherwise = (a,b)
+
+-- compareTriplets a b = pairToList $ tripletCompare (Data.List.zip a b) (0,0)
+compareTriplets a b = pairToList $ foldr tripletCompare (0,0) $ zip a b 
 pairToList (a,b) = [a,b]
