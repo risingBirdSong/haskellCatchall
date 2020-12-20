c72b739b848f6f4e155dc7f154252f08e51b7f8b
risingbirdsong <heregoingthere@outlook.com>
2020-12-19 18:18:56 -0800

    solved chunky monkey with Bernie :)

--- src/scrimba.hs
+++ src/scrimba.hs
@@ -1,6 +1,12 @@
 import Data.List
+import Data.List.Split
 import Data.Ord
 strs = ["abc", "", "aaa", "a", "zz"];
 
 -- sortByLength ls = sortBy (comparing length) ls
+values = ["a", "b", "c", "d","e", "f"];
 sortByLength ls = sortOn length ls 
+
+
+chunkyMonkey chnk [] = []
+chunkyMonkey chnk lst = (take chnk lst) : chunkyMonkey chnk (drop chnk lst)
