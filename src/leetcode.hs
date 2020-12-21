7175f0be794d15d4614a15055980908c59206588
risingbirdsong <heregoingthere@outlook.com>
2020-12-20 19:57:07 -0800

    solved fair candy, I made that harder than it needed to be. but arrived at an answer that makes sense

--- src/leetcode.hs
+++ src/leetcode.hs
@@ -1,11 +1,26 @@
+import Debug.Trace
+import Data.List
 a = [1,1]
 b = [2,2]
--- fairCandy as bs = [small, big]
-      -- where small = 
+c = [1,2] 
+d = [2,3]
+
+e = [2]
+f = [1,3]
+
+g = [1,2,5]
+h = [2,4]
+
+fairCandy as bs = go
+  where avg = (sum as + sum bs) `div` 2
+        go = [(a,b) | a <- as, b <- bs, ((sum as) - a + b) == avg]
+             
+
 
 smaller as bs = head $ filter (\x -> (sum x) == (min (sum as) (sum bs))) [as,bs]
 
 smaller_ as bs 
-  | sum as < sum bs = as 
+  | sum as < sum bs = as
   | otherwise = bs 
 
+theOtherList theOne choice@[as,bs] = head $ filter ((/=) theOne ) choice 
