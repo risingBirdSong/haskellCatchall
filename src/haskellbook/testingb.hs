7506dba61d6db24381e370c75885871035d514c1
risingbirdsong <heregoingthere@outlook.com>
2020-12-29 14:15:41 -0800

    quick check helps reveal the problem, fixed it by moving the empty pattern match and letting the recursion handle it, then all quickcheckspass

--- src/haskellbook/testingb.hs
+++ src/haskellbook/testingb.hs
@@ -1,5 +1,6 @@
 import Test.QuickCheck
 import Data.List
+import qualified Data.List.Split as S
 import Debug.Trace
 prop_revapp :: [Int] -> [Int] -> Bool
 prop_revapp xs ys = reverse (xs++ys) == reverse ys ++ reverse xs
@@ -15,7 +16,7 @@ mySplit' d ls = tk : (if null dp then [] else  mySplit' d (tail dp) )
             dp = dropWhile (/=d) ls
 
 
-split c [] = []
+-- split c [] = []
 split c xs = xs' : if null xs'' then [] else split c (tail xs'')
     where xs' = takeWhile (/=c) xs
           xs''= dropWhile (/=c) xs
@@ -33,9 +34,21 @@ unsplit c = concat . intersperse [c]
 
 
 
-prop_split_inv c xs = unsplit c (split c xs) == xs
+prop_split_invA c xs = unsplit c (split c xs) == xs
 
-maintest = quickCheck prop_split_inv
+maintestA = quickCheck prop_split_invA
+
+prop_split_invB c xs 
+    = let ys = split c xs in 
+      collect (length ys) $ unsplit c ys == xs
+
+maintestB = quickCheck prop_split_invB
+
+prop_split_invC xs
+    = forAll (elements xs) $ \c -> 
+      unsplit c (split c xs) == xs
+
+maintestC = quickCheck prop_split_invC
 
 -- *Main> maintest
 -- +++ OK, passed 100 tests.
