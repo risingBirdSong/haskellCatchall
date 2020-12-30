593345a06436173cd47a5a5c07eba18fc9e987a4
risingbirdsong <heregoingthere@outlook.com>
2020-12-29 20:41:03 -0800

    an updated version of printTable expert version

--- src/tableB.hs
+++ src/tableB.hs
@@ -3,3 +3,9 @@ printTable table =
   where
   largest = maximum $ map (length . show) (concat table)
   pad str = replicate (max (largest - length str) 0) ' ' ++ str
+
+printTableB table = 
+  putStrLn $ unlines $ map (unwords . map (pad . show)) table
+  where
+  largest = maximum $ map (length . show) (concat table)
+  pad str = replicate (max (largest - length str) 0) ' ' ++ str
