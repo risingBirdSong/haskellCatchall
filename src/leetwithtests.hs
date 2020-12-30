515fdd4813c8672e36d13684e41c2e0353eb97f6
risingbirdsong <heregoingthere@outlook.com>
2020-12-30 08:57:06 -0800

    very interesting and good stuff. wrote a basic sanity check for greatestcandy function, very simple, just checking to make sure that the maximum number in the input to grtscandy is True, and the lowest number is False, which made sense to me, and conformed to the leetcode examples. and a good simple starting posistion for practicing tests. and then on the testing side, i constrained it to ensure that there would be at least one false input. and this works, but I noticed that some of the numbers were negative and wanted only positive ones. Talking with Edmund, how to constrain with both special types, nonempty and nonnegative, and he gave me a hint that im using lists, but that wont work, i need a higher level of abstraction, foldable, perfect motivation to keep reading the book to get to those higher abstractions!

--- src/leetwithtests.hs
+++ src/leetwithtests.hs
@@ -16,12 +16,18 @@ invsRngSum ls = inverseScan (+) (rngSum ls)
 runningSumCheck ls = ls == invsRngSum ls     
 runningSumTest = verboseCheck (runningSumCheck :: [Int] -> Bool)
 
+-- grtstCandy :: NonEmptyList (NonNegative Integer) -> (NonNegative Integer) -> [Bool]
+-- grtstCandy :: (Ord a, Num a) => [a] -> a -> [Bool]
 grtstCandy ls e = map (\x -> x + e >= maximum ls) ls 
 
-grtstex =  grtstCandy [2,3,5,1,3] 3
+-- grtstex =  grtstCandy [2,3,5,1,3] 3
+-- grtstrsmax :: NonEmptyList ([NonNegative Integer]) -> (NonNegative Integer) -> Bool
 grtstrsmax ls e = True == (grtstCandy ls e !! fromMaybe 0 ( elemIndex (maximum ls) ls)) 
+-- grtstrsmin :: (Ord a, Num a) => [a] -> a -> Bool
 grtstrsmin ls e = False  == (grtstCandy ls e !! fromMaybe 0 ( elemIndex (minimum ls) ls)) 
 
 grtsttest :: (NonEmptyList Integer) -> (NonNegative Integer) -> Property 
+-- grtsttest :: (NonEmptyList (NonNegative Integer)) -> (NonNegative Integer) -> Property 
+-- grtsttest :: (Ord a, Num a) => NonEmptyList a -> NonNegative a -> Property
 grtsttest (NonEmpty ls) (NonNegative e) = (minimum ls + e < maximum ls)  ==> (grtstrsmax ls e) == True && (grtstrsmin ls e ) == True 
 grtstcheck = verboseCheck (grtsttest)
--- src/tableB.hs
+++ src/tableB.hs
@@ -4,6 +4,7 @@ printTable table =
   largest = maximum $ map (length . show) (concat table)
   pad str = replicate (max (largest - length str) 0) ' ' ++ str
 
+
 printTableB table = 
   putStrLn $ unlines $ map (unwords . map (pad . show)) table
   where
