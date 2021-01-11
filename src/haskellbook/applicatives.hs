84388c8e1f31bdd46f5defc97b586dc3303b7992
risingbirdsong <heregoingthere@outlook.com>
2021-01-10 21:27:12 -0800

    mostly: a good example of how useful Applicative is

--- src/haskellbook/applicatives.hs
+++ src/haskellbook/applicatives.hs
@@ -42,3 +42,58 @@ yy = lookup 2 $ zip xs ys
 -- summed :: Maybe Integer
 summed = uncurry max <$> liftA2 (,) xx yy
 
+newtype Identity a = Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a) 
+
+instance Applicative Identity where
+  pure v = Identity v  
+  (<*>) (Identity f) (Identity v)  = Identity (f v  )
+
+newtype Constant a b =
+  Constant { getConstant :: a }
+  deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant a) = Constant a
+
+instance Monoid a => Applicative (Constant a) where
+  pure _ = Constant { getConstant = mempty }
+  (<*>) (Constant x) (Constant y) = Constant (mappend x y)
+
+validateLength :: Int -> String -> Maybe String
+validateLength maxLen s =
+  if (length s) > maxLen
+  then Nothing
+  else Just s
+
+newtype Name = Name String deriving (Eq, Show)
+newtype Address = Address String deriving (Eq, Show)
+
+mkName :: String -> Maybe Name
+mkName s = fmap Name $ validateLength 10 s
+mkAddress :: String -> Maybe Address
+mkAddress a = fmap Address $ validateLength 10 a
+
+data Person =
+  Person Name Address
+  deriving (Eq, Show)
+
+mkPerson :: String -> String -> Maybe Person
+mkPerson n a =
+  case mkName n of
+    Nothing -> Nothing
+    Just n' ->
+      case mkAddress a of
+        Nothing -> Nothing
+        Just a' ->
+          Just $ Person n' a'
+
+
+maybeAddy = (mkAddress "old macs")
+
+mkPersonBetter :: String -> String -> Maybe Person
+mkPersonBetter n a =
+  Person <$> mkName n <*> mkAddress a
