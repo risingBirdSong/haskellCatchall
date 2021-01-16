303537aa65929aade219d409862cf37b199cf735
risingbirdsong <heregoingthere@outlook.com>
2021-01-16 12:30:38 -0800

    my first attempt at twoTail >>= for Sum type

--- src/haskellbook/monad.hs
+++ src/haskellbook/monad.hs
@@ -80,3 +80,21 @@ mkSoftware years coders = do
   if programmers > div founded 10
   then Left $ TooManyCodersForYears founded programmers
   else Right $ Shop founded programmers
+
+data Sum a b =
+  First a
+  | Second b
+  deriving (Eq, Show)
+instance Functor (Sum a) where
+  fmap f (Second b) = Second (f b) 
+
+instance Applicative (Sum a) where
+  pure val = Second val 
+  (<*>) (First a) _ = (First a)
+  (<*>)  _ (First b) = (First b)
+  (<*>) (Second f) (Second b) = (Second (f b)) 
+
+instance Monad (Sum a) where
+  return = pure  
+  (>>=) mn f = join (fmap f mn) 
+
