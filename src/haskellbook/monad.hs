de65b25aa72643b20a582680cb4f365507ded47a
risingbirdsong <heregoingthere@outlook.com>
2021-01-15 19:14:53 -0800

    how to inspect the type with a specific instance applied to it... and a little more monad learning

--- src/haskellbook/monad.hs
+++ src/haskellbook/monad.hs
@@ -13,3 +13,28 @@ bind f mon  = join $ fmap f mon
 --   (>>=) :: m a -> (a -> m b) -> m b
 --   (>>) :: m a -> m b -> m b
 --   return :: a -> m a
+
+say1 = putStrLn $ show 1 
+say2 = putStrLn $ show 2 
+
+saytup = (say1, say2)
+-- *Main> fst saytup
+-- 1
+-- *Main> snd saytup
+-- 2
+
+-- (>>=) :: [a] -> (a -> [b]) -> [b]
+
+twiceWhenEven :: [Integer] -> [Integer]
+twiceWhenEven xs = do
+  x <- xs
+  if even x
+  then [x*x, x*x]
+  else []
+
+-- *Main> twiceWhenEven [1..3]
+-- [4,4]
+
+-- how to inspect a certain specific instance of a type 
+--  :set -XTypeApplications
+--  :t (>>=) @Maybe
