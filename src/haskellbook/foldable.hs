b614b39b50d62a7d1b018b119e2204c6cb6b0676
risingbirdsong <heregoingthere@outlook.com>
2021-01-19 11:32:34 -0800

    oh cool... I tried to use mempty to see if its nullish, and cant use mempty because its not constrained to monoid, but then constrained it like so, and it worked for basic cases...

--- src/haskellbook/foldable.hs
+++ src/haskellbook/foldable.hs
@@ -16,5 +16,19 @@ myelem :: (Foldable t, Eq a) => a -> t a -> Bool
 myelem = any . (==)
 
 -- myminimum xs = foldr (min) (1/0) xs
+-- 9223372036854775807 is the highest max bound for Int
+-- myminimum xs = foldr (min) (9223372036854775807) xs
 myminimum [] = Nothing
-myminimum xs = Just (foldr (min) (head xs) xs)
+myminimum xs = Just (foldr (min) (head xs))
+
+-- the official implementation using a newtype and coercible, found in utilities
+-- minimum = fromMaybe (errorWithoutStackTrace "minimum: empty structure") .
+--     getMin . foldMap (Min #. (Just :: a -> Maybe a))
+
+mymaximum [] = Nothing
+mymaximum xs = Just (foldr max 0 xs)
+
+mynull :: (Foldable t, Eq (t a), Monoid (t a)) => t a -> Bool
+mynull xs 
+  | xs == mempty = True
+  | otherwise = False
