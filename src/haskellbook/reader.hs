aa190ea15e6475cb7195b14b9e259edf8d8f4d7b
risingbirdsong <heregoingthere@outlook.com>
2021-01-26 16:54:56 -0800

    into the chapter exercises  and it's dinner time

--- src/haskellbook/reader.hs
+++ src/haskellbook/reader.hs
@@ -4,6 +4,8 @@ import Control.Monad
 
 import Data.Char
 import Data.List
+import Data.Maybe
+
 boop = (*2)
 doop = (+10)
 -- bip :: Num a => a -> a 
@@ -162,3 +164,28 @@ instance Monad (Reader r) where
 
 -- Reader $ \r -> undefined
 
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+
+-- xs :: Maybe Integer
+xs = lookup 3 $ zip x y 
+
+ys :: Maybe Integer
+ys = lookup 6 $ zip x y
+
+z' :: Integer -> Maybe Integer
+z' n = lookup n $ zip x y
+
+x1 :: Maybe (Integer, Integer)
+x1 =  (,) <$> xs <*> xs 
+x1' :: Maybe (Integer, Integer)
+x1' =  (,) <$> xs <*> ys 
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> xs
+
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
