262169949942047bce429f6d9e9176f9d000a52c
risingbirdsong <heregoingthere@outlook.com>
2021-01-27 16:55:49 -0800

    not so fast, just because it compiles doesnt mean its valid, i was wrongly throwing away state, but this is the way to pass on the next state, makes much more sense

--- src/haskellbook/state3.hs
+++ src/haskellbook/state3.hs
@@ -8,3 +8,17 @@ newtype State s a =
 instance Functor (State s) where 
   fmap :: (a-> b) -> State s a -> State s b
   fmap f (State g) = State (\s -> (\(a,s') -> (f a , s')) $ g s ) 
+
+-- *Main> runState ((+1) <$> (State  $ \s -> (0,s))) 0
+-- (1,0)
+
+-- (<*>) :: State s (a -> b)
+-- -> State s a
+-- -> State s b
+instance Applicative (State s) where 
+  pure x = State (\s -> (x, s))
+  (<*>) (State sab) (State sa) = State (\s ->
+    let (a, s') = sa s 
+        (f, s'') = sab s'
+    in (f a, s'') 
+    )
