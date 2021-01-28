aae7c60dd0ef8d8a74f29e025d6a04a5185e2f94
risingbirdsong <heregoingthere@outlook.com>
2021-01-28 11:57:09 -0800

    mostly just studying the code to understand how its working... also there was a task to figure out how to rewrite it so as not to need reverse, and no dlist, so I just ran from 100 to 1 instead of 1 to 100

--- src/haskellbook/statefizzbuzz.hs
+++ src/haskellbook/statefizzbuzz.hs
@@ -1,3 +1,22 @@
 import Control.Monad
-import Control.Monad.State.Lazy
+import Control.Monad.Trans.State
 
+-- newtype MyState s a = MyState {runState :: s -> (a,s)}
+
+fizzBuzz :: Integer -> String
+fizzBuzz n | n `mod` 15 == 0 = "FizzBuzz"
+           | n `mod` 5 == 0 = "Fizz"
+           | n `mod` 3 == 0 = "Buzz"
+           | otherwise = show n
+fizzbuzzList :: [Integer] -> [String]
+fizzbuzzList list =
+  execState (mapM_ addResult list) []
+-- addResult :: Integer -> StateT [String] 
+addResult n = do
+  xs <- get
+  let result = fizzBuzz n
+  put (result : xs)
+-- Note that State is a type alias of StateT you imported.
+main :: IO ()
+-- main = mapM_ putStrLn $ reverse $ fizzbuzzList [1..100]
+main = mapM_ putStrLn $ fizzbuzzList [100,99..1]
