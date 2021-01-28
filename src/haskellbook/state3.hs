a56a991fad7a2f7b88badb571c18f6a0af16c698
risingbirdsong <heregoingthere@outlook.com>
2021-01-27 21:20:45 -0800

    blessing to the channel, they helped me out big time, I was close in idea, but the unwrapping with runState, that was missing and wasnt even on my radar... and providing the nice examples of how its used. grateful

--- src/haskellbook/state3.hs
+++ src/haskellbook/state3.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE InstanceSigs #-}
 
 import System.Random
+import Control.Monad
 
 newtype State s a = 
   State {runState :: s -> (a,s)}
@@ -17,8 +18,33 @@ instance Functor (State s) where
 -- -> State s b
 instance Applicative (State s) where 
   pure x = State (\s -> (x, s))
-  (<*>) (State sab) (State sa) = State (\s ->
-    let (a, s') = sa s 
-        (f, s'') = sab s'
+  (<*>) (State sab) (State sa) = State $ \s ->
+    let (f, s') = sab s
+        (a, s'') = sa s' 
     in (f a, s'') 
-    )
+    
+
+-- incState :: State Int Int
+incState = State $ \s -> (s + 1, s + 1)
+
+ran = runState ((,,,) <$>  incState <*> incState <*> incState <*> incState) 0
+
+  -- (>>=) :: State s a
+  --   -> (a -> State s b)
+  --   -> State s b
+instance Monad (State s) where
+  return = pure
+  (State f) >>= g = State $ \s ->
+      let (a, s') = f s 
+          ssb = g a 
+      in runState ssb s' 
+
+      -- pattern matching with State, is like what runState is doing, simply unwrapping
+      -- let (a, s') = f s
+      --   State ssb = g a 
+      --   (b, s'') = ssb s'
+      -- in (b, s'') 
+statemonadexample =  runState (do { x <- incState; y <- replicateM x incState; return (x, y) }) 5
+
+-- *Main> statemonadexample
+-- ((6,[7,8,9,10,11,12]),12)
