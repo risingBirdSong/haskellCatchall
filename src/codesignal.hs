e7aee0bbac0df3e0c58f60f90ac77db23850af5e
risingbirdsong <heregoingthere@outlook.com>
2021-02-05 10:41:00 -0800

    my first first go at firstDuplicate from codeSignal, great challenge! was a challenge and good learning opportunity :)

--- src/codesignal.hs
+++ src/codesignal.hs
@@ -1,7 +1,13 @@
 -- https://app.codesignal.com/interview-practice/task/pMvymcahZ8dY4g75q/description
+-- {-# LANGUAGE FlexibleContexts #-}
+
 import Data.List
+import Data.Ord
 
 firstDuplicateNestedlambdas xs = groupBy (\(x, v1) -> \(y,v2) -> x == y ) . sort $ zip xs [0..]  
 firstDuplicateCollapsed xs = groupBy (\(x, v1) (y,v2) -> x == y ) . sort $ zip xs [0..]  
 
-firstDuplicate xs = groupBy (\(x, v1) (y,v2) -> x == y ) . sort $ zip xs [0..]  
+firstDuplicate xs = solve go 
+  where go =  sortBy (\x y -> compare (snd $ last x) (snd $ last y)) . filter ((>1).length) . groupBy (\(x, v1) (y,v2) -> x == y ) . sort $ zip xs [0..]  
+        solve [] = -1
+        solve ([(x,i), _] : xs) = x
