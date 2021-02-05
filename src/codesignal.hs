8cc4d6c81042d2ee025f8e885043aec44fdf62ff
risingbirdsong <heregoingthere@outlook.com>
2021-02-05 09:54:12 -0800

    an example of groupingBy an element inside a tuple, with the two lambdas, but hlint shows me a new concept, called 'collapsed lambdas where you can put the two separate inputs in one lambda, like demonstrated, nice one'

--- src/codesignal.hs
+++ src/codesignal.hs
@@ -1,5 +1,7 @@
 -- https://app.codesignal.com/interview-practice/task/pMvymcahZ8dY4g75q/description
 import Data.List
 
-firstDuplicate xs = groupBy (\(x, v1) -> \(y,v2) -> x == y ) . sort $ zip xs [0..]  
+firstDuplicateNestedlambdas xs = groupBy (\(x, v1) -> \(y,v2) -> x == y ) . sort $ zip xs [0..]  
+firstDuplicateCollapsed xs = groupBy (\(x, v1) (y,v2) -> x == y ) . sort $ zip xs [0..]  
 
+firstDuplicate xs = groupBy (\(x, v1) (y,v2) -> x == y ) . sort $ zip xs [0..]  
