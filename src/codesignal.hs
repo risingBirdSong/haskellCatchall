4c1ac04110a58bef741628e067cf8391a405c2d4
risingbirdsong <heregoingthere@outlook.com>
2021-02-05 11:11:48 -0800

    was getting a non exhaustive pattern matching bug and made a simple pattern matching for tuples to test, and duh! was making a simple error with pattern matching, the answer is making the explicit pattern matches for tuples we care about and then using the _ catchall for tuples we dont care about, I was using incorrectly like shown in the commentted out code

--- src/codesignal.hs
+++ src/codesignal.hs
@@ -2,12 +2,20 @@
 -- {-# LANGUAGE FlexibleContexts #-}
 
 import Data.List
-import Data.Ord
+-- import Data.Ord
 
 firstDuplicateNestedlambdas xs = groupBy (\(x, v1) -> \(y,v2) -> x == y ) . sort $ zip xs [0..]  
 firstDuplicateCollapsed xs = groupBy (\(x, v1) (y,v2) -> x == y ) . sort $ zip xs [0..]  
 
-firstDuplicate xs = solve go 
+firstDuplicate xs = solve $ go 
   where go =  sortBy (\x y -> compare (snd $ last x) (snd $ last y)) . filter ((>1).length) . groupBy (\(x, v1) (y,v2) -> x == y ) . sort $ zip xs [0..]  
         solve [] = -1
-        solve ([(x,i), _] : xs) = x
+        solve ([(x,i), _] : _) = x
+
+firstDInnerLogic xs =   sortBy (\x y -> compare (snd $ last x) (snd $ last y)) . filter ((>1).length) . groupBy (\(x, v1) (y,v2) -> x == y ) . sort $ zip xs [0..]
+
+
+tupleMatching [(x,y)] = "oneTuple"
+tupleMatching [(x,y),(xx,yy)] = "twoTuple"
+tupleMatching _ = "moreThanTwoTuples"
+
