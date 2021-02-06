26192b56b31c58cbbd6848eb9d769901803a41aa
risingbirdsong <heregoingthere@outlook.com>
2021-02-05 16:09:36 -0800

    solved firstNotRepeatingCharacter in another way, good for more learning and practice

--- src/codeSignalRefactor.hs
+++ src/codeSignalRefactor.hs
@@ -1,5 +1,7 @@
 import Data.List
 import qualified Data.Set as S
+import Data.Foldable
+import Data.Function (on)
 
 
 firstDuplicate as = go as S.empty   
@@ -7,3 +9,25 @@ firstDuplicate as = go as S.empty
         go (x:xs) set 
           | S.member x set = x
           | otherwise = go xs (S.insert x set)
+
+maximumOn :: (Foldable t, Ord b) => (a -> b) -> t a -> a
+maximumOn f = maximumBy (compare `on` f)
+
+minimumOn :: (Foldable t, Ord b) => (a -> b) -> t a -> a
+minimumOn f = minimumBy (compare `on` f)
+
+
+-- firstNotRepeatingCharacter "" = '_'
+firstNotRepeatingCharacter str = go
+   where go = solve $ findmins prepare 
+         prepare = concat . filter ((==1).length)
+          . groupBy (\(x,_)(y,_) -> x == y) 
+          $ sort $ zip str [0..]
+         findmins [] = Nothing  
+         findmins vals = Just ( fst $ minimumOn snd vals) 
+         solve Nothing = '_'
+         solve (Just x) = x
+
+fnrcLogic str = concat . filter ((==1).length)
+          . groupBy (\(x,_)(y,_) -> x == y) 
+          $ sort $ zip str [0..]
