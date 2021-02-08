ca5b4d8f3c7adfc5af9a35f45e46612c55331647
risingbirdsong <heregoingthere@outlook.com>
2021-02-08 13:01:02 -0800

    researched for my use case and found extremely useful function, zipWithPadding. adding to my library

--- src/mapdict.hs
+++ src/mapdict.hs
@@ -6,12 +6,18 @@ import qualified Data.List.HT as HT
 -- https://leetcode.com/problems/merge-two-binary-trees/
 -- 617. Merge Two Binary Trees
 
+-- mtbs [] [] = []
+-- mtbs [] ()
+
 -- root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
-a = map myConvert [1,3,2,5]
+a =  [1,3,2,5]
+b =  [2,1,3,0,4,0,7]
+
+
+
 ma = [Just 1,Just 3,Just 2,Just 5]
 mb = [Just 2,Just 1,Just 3, Nothing ,Just 4,Nothing,Just 7]
 -- [Just 1,Just 3,Just 2,Just 5]
-b = map myConvert [2,1,3,-1,4,-1,7]
  
 -- [Just 2,Just 1,Just 3,Nothing,Just 4,Nothing,Just 7]
 myConvert n 
@@ -23,5 +29,13 @@ mybZip ((Just x):xs) ((Just y):ys) = Just (x, y) : mybZip xs ys
 mybZip _  _ = [] 
 
 apZip :: (Num a) =>  [Maybe a] -> [Maybe a] -> [Maybe (a, a)]
-apZip xs ys = zipWith (liftA2 (,)) [Just 1] [Nothing]
+apZip = zipWith (liftA2 (,))
 
+
+zipWithPadding :: a -> b -> [a] -> [b] -> [(a,b)]
+zipWithPadding a b (x:xs) (y:ys) = (x,y) : zipWithPadding a b xs ys
+zipWithPadding a _ []     ys     = zip (repeat a) ys
+zipWithPadding _ b xs     []     = zip xs (repeat b)
+
+--  HT.takeWhileJust (apZip ma mb)
+-- [(1,2),(3,1),(2,3)]
--- src/mylibrary.hs
+++ src/mylibrary.hs
@@ -12,6 +12,14 @@ filterTwo f (x:y:ls)
   | otherwise = filterTwo f (y:ls)
 
 
+-- https://stackoverflow.com/questions/22403029/how-to-zip-lists-with-different-length
+-- brilliant helper function from the community
+-- because sometimes you want to your zip to keep going till both lists are empty
+-- so just fill the padding in with mempty ish type values
+zipWithPadding :: a -> b -> [a] -> [b] -> [(a,b)]
+zipWithPadding a b (x:xs) (y:ys) = (x,y) : zipWithPadding a b xs ys
+zipWithPadding a _ []     ys     = zip (repeat a) ys
+zipWithPadding _ b xs     []     = zip xs (repeat b)
 
 -- a helper function I wrote wanting to zip when the values are Just, otherwise stop zipping
 maybeZip ((Just x):xs) ((Just y):ys) = (Just x, Just y) : maybeZip xs ys
