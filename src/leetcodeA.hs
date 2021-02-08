aa765944c53c4f90f4949bbb1ac58a9f2dfe6370
risingbirdsong <heregoingthere@outlook.com>
2021-02-08 15:46:30 -0800

    a much sweeter idea, but note i would have to handle even or odd cases of the length to account for the final step

--- src/leetcodeA.hs
+++ src/leetcodeA.hs
@@ -1,5 +1,5 @@
 import Data.List 
-
+import Data.List.Split
 clmbA =  [1 , 100, 1, 1, 1, 100, 1, 1, 100, 1]
 
 climbing [] steps = []
@@ -8,5 +8,8 @@ climbing (cur:[x]) steps = [cur]
 climbing (cur:x:y:zs) steps 
   | x < y =  cur : climbing (x:y:zs) (steps + 1)
   | x >= y = cur : climbing (y:zs) (steps + 1)
-
 climbSolve (path) = minimum $ map (sum) [(climbing (drop 1 path) 0) , (climbing path 0)] 
+
+
+climbing' xs = sum . map minimum $ chunksOf 2 xs
+climbingSolve' xs = min (climbing' xs) (climbing' (drop 1 xs))
