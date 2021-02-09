43090270672ab9cbdb5bd9cac087b9d64353d262
risingbirdsong <heregoingthere@outlook.com>
2021-02-08 21:52:34 -0800

    reading article about dynamic programming and understand more why linearFib is 0n, naiveFib is exponential, and why linearFib is DP

--- src/dynamicprog.hs
+++ src/dynamicprog.hs
@@ -1,3 +1,17 @@
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n-1) + fib (n -2)
+naivefib 0 = 0
+naivefib 1 = 1
+naivefib n = naivefib (n-1) + naivefib (n -2)
+
+naiveLCS :: String -> String -> String
+naiveLCS [] _ = []
+naiveLCS _ [] = []
+naiveLCS (x:xs) (y:ys)
+  | x == y    = x : naiveLCS xs ys
+  | otherwise = max (naiveLCS (x:xs) ys) (naiveLCS xs (y:ys))
+
+linearFib n = let fib = 0 : 1 : zipWith (+) fib (tail fib) in fib !! n
+
+linearFib' n = let fib = 0 : 1 : zipWith (+) fib (tail fib) in fib !! n
+
+-- *Main> naiveLCS "nematode knowledge" "empty bottle"
+-- "tole"
