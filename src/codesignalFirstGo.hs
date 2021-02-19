a933ffc67e8bf97f24ae588caf6b03d0b95e8058
risingbirdsong <heregoingthere@outlook.com>
2021-02-19 09:34:12 -0800

    a nice and simple printy printer with debug.trace, and empty string to show and then just returning x

--- src/codesignalFirstGo.hs
+++ src/codesignalFirstGo.hs
@@ -835,4 +835,23 @@ digits' = map digitToInt . show
 -- bishopAndPawn bishop pawn = 
 
 -- with list comprehensions, remember the fast moving / inner loop is the on the right hand side, like nm below...
-diagmoves bshp = [ (ltr,nm) |  ltr <-['a'..'h'] ,nm <- [1..8]]
+diagmoves = chunksOf 8 [ (ltr,nm) |  nm <- [1..8] , ltr <-['a'..'h']] 
+-- bottom to top, fast rows
+trnsp1 = transpose diagmoves
+-- bottom to top , fast cols 
+
+diag = zip diagmoves (tail diagmoves)
+
+
+prtPrint mtr = map (\x -> trace  "" x ) mtr
+
+-- *Main> prtPrint diagmoves
+-- [
+-- [('a',1),('b',1),('c',1),('d',1),('e',1),('f',1),('g',1),('h',1)],
+-- [('a',2),('b',2),('c',2),('d',2),('e',2),('f',2),('g',2),('h',2)],
+-- [('a',3),('b',3),('c',3),('d',3),('e',3),('f',3),('g',3),('h',3)],
+-- [('a',4),('b',4),('c',4),('d',4),('e',4),('f',4),('g',4),('h',4)],
+-- [('a',5),('b',5),('c',5),('d',5),('e',5),('f',5),('g',5),('h',5)],
+-- [('a',6),('b',6),('c',6),('d',6),('e',6),('f',6),('g',6),('h',6)],
+-- [('a',7),('b',7),('c',7),('d',7),('e',7),('f',7),('g',7),('h',7)],
+-- [('a',8),('b',8),('c',8),('d',8),('e',8),('f',8),('g',8),('h',8)]]
