37566bb13f7522ee68763306e26620a15d5bcf6b
risingbirdsong <heregoingthere@outlook.com>
2021-02-23 12:55:39 -0800

    new strategy and new hlper function for file naming challenge

--- src/codesignalA.hs
+++ src/codesignalA.hs
@@ -9,27 +9,96 @@ foldCount xs = foldr logic (M.empty) xs
               | M.member x mp = M.adjust (+1) x mp
               | otherwise = M.insert x 1 mp 
 
+names = ["a(1)", 
+ "a(6)", 
+ "a", 
+ "a", 
+ "a", 
+ "a", 
+ "a", 
+ "a", 
+ "a", 
+ "a", 
+ "a", 
+ "a"]
+
 files = ["doc", "doc", "image", "doc(1)", "doc"]
-countFiles files = go files (M.empty)
-  where go [] mp = []
-        go (f:fs) mp 
-              | M.member f mp = case (M.lookup f mp) of 
-                                      Just x 
-                                         | x == 1 -> (f ++ "(" ++ (show x) ++ ")") : go fs (M.adjust (+1) f mp)
-                                         | x > 1 -> (f ++ "(" ++ (show x) ++ ")(" ++ (show x) ++ ")") : go fs (M.adjust (+1) f mp)
-                                      Nothing -> go fs (M.insert "error" 1 mp)
-              | otherwise = f : go fs (M.insert f 1 mp)
 
-aa =  [[4, 1],
-                [4, -1],
-                [0, 0],
-                [6, 1]]
-
-bb = [[3,1], 
-      [6,1], 
-      [4,1], 
-      [5,1]]
 
 companyBotStrategy trn =  case (map head $ filter (\[v,crct] -> crct == 1) trn) of 
                                 [] -> 0.0
                                 xs -> (realToFrac $ sum xs) / (realToFrac $ length xs)
+
+testMatrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+  ]
+
+diagsA mtrx = zipWith (\blnks rows -> blnks ++ map (:[]) rows) (iterate ([]:) []) mtrx
+
+-- fileNames fls 
+
+fnames = ["doc", 
+        "doc", 
+        "image", 
+        "doc(1)", 
+        "doc"]
+
+--           0                1                       2               3              4
+-- myMap -> (doc : 1) -> (doc : 2, doc(1) : 1) ->  (image : 1) -> (doc(1) : 2) ->  (doc : 3 )
+-- output -> "doc"          "doc(1)"                "image"         doc(1)(1)         doc(2)
+
+
+fileNames files = go files (M.empty) 
+  where go  []  mp = []
+        go (f:fs) mp 
+          | M.member f mp = prnsPrint : go fs ( M.adjust (+1) f (M.insert prnsPrint 1 mp))
+          | otherwise = f : go fs (M.insert f 1 mp) 
+            where fAppnd f n = f ++ "(" ++ show n ++ ")" 
+                  lkp k = case (M.lookup k mp) of
+                    Just x -> x
+                  prnsPrint = (fAppnd f (lkp f))
+
+-- fAppnd f n = f ++ "(" ++ show n ++ ")"  
+
+-- ["a(1)", 
+--  "a(6)", 
+--  "a", 
+--  "a", 
+--  "a", 
+--  "a", 
+--  "a", 
+--  "a", 
+--  "a", 
+--  "a", 
+--  "a", 
+--  "a"]
+
+-- ["a(1)", 
+--  "a(6)", 
+--  "a", 
+--  "a(2)", 
+--  "a(3)", 
+--  "a(4)", 
+--  "a(5)", 
+--  "a(7)", 
+--  "a(8)", 
+--  "a(9)", 
+--  "a(10)", 
+--  "a(11)"]
+
+-- im thinking of a better
+-- way to store multiple encounters in the Map above, rather than store multiple copies of a file which is confusing,
+-- store a single file as the key, and its value will be a list of all the dupelicates that have been encountered
+-- example above on line 65 with the list of as... store only a single a, with a list of the already taken numbers
+-- so follow the iterations ->
+-- (a: [1]) -> (a: [1,6]) -> (a : [0,1,6]) -> (a : [0,1,2,6]) ... etc
+-- notice that the third a gets output as simple "a" because zero wasnt taken yet.
+
+-- below is a helper function to find the next occurence
+nextNumber taken = go taken [0..]
+  where go [] (n:ns) = (n, (sort (n:taken)))
+        go (t:tkn) (n:ns) 
+          | n < t = (n, sort (n:taken))
+          | n == t = go tkn [(n+1)..]
