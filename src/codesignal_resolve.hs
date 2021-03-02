61a81e735c4d0a76878f9ede65266a123c19c4f9
risingbirdsong <heregoingthere@outlook.com>
2021-03-02 15:51:11 -0800

    here have libGroupBy which is copy past from the special lib that provides this alternative...

--- src/codesignal_resolve.hs
+++ src/codesignal_resolve.hs
@@ -17,5 +17,23 @@ makeArrayConsecutive2 n = length [(minimum n) .. (maximum n)] - length n
 -- almostIncreasingSequence s = and $ ((<2) . length . filter (uncurry (>=)) . zip s) . tail <$> [s, tail s]
 
 
-almostIncreasingSequence s = and $ ((<2) . length . filter (uncurry (>=)) . zip s) . tail <$> [s, tail s]
+almostIncreasingSequence s = (filter (uncurry (>=)) . zip s) . tail <$> [s, tail s]
 
+la = ["aba", 
+ "aa", 
+ "ad", 
+ "vcd", 
+ "aba"]
+
+libgroupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+libgroupBy _ [] = []
+libgroupBy p' (x':xs') = (x' : ys') : zs'
+  where
+    (ys',zs') = go p' x' xs'
+    go p z (x:xs)
+      | p z x = (x : ys, zs)
+      | otherwise = ([], (x : ys) : zs)
+      where (ys,zs) = go p x xs
+    go _ _ [] = ([], [])
+
+allLongestStrings xs = last $ libgroupBy (\x y -> length x == length y) $ sortBy (comparing length) xs
