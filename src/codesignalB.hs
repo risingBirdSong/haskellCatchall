9e5f4af2c601df64a4d95c37d68c820c016ea560
risingbirdsong <heregoingthere@outlook.com>
2021-03-10 14:44:57 -0800

    solved deleteDigit,first try in a while

--- src/codesignalB.hs
+++ src/codesignalB.hs
@@ -1,5 +1,8 @@
 import Data.List
 import Data.List.Split
+import Data.Char
+import Data.Function
+import Data.Ord
 buildPalindrome xs =  concat $ concatMap (nub) $ group $ (group xs) ++ (group $  reverse xs)
 buildPalindrome' xs =  (group xs) ++ (group $  reverse xs)
 
@@ -25,3 +28,29 @@ lineEncoding str = concatMap lgc $ group str
   where lgc [xs] = [xs]
         lgc s = (show $ length s) ++ [head s] 
           
+
+
+board = concatMap (\n -> zipWith (\a b -> (fromEnum a) + (fromEnum b)) (['A'..'Z']) (repeat n)) [1..8] 
+
+
+deleteDigit n = stichBackDeleted $ deleter $ numstoList n 
+numstoList n = map digitToInt $ show n 
+listtoint ns =  read(map (intToDigit) ns) :: Int
+        
+deleter ns = filter ((/= (length ns)).length) $ snd $ mapAccumL (\acc list -> (acc+1, delete acc list)) 0 (replicate (length ns + 1) ns)
+    where zipped = zip [0..] ns
+
+stichBackDeleted xss = maximum $ map (listtoint) xss
+
+
+-- longestWord txt = maximumBy (comparing length) $ map (filter (isAlpha)) $ words txt
+
+
+longestWord txt = maximumBy (comparing length) $ splitWhen (not.isAlpha) txt
+
+handleTime "24" "00" = False 
+handleTime h' m' = ( h >= 0 && h <= 24) && (m >= 0 && m <= 60) && (head h' /= '0')
+      where h = (read h' :: Int)
+            m = (read m' :: Int)
+            
+validTime time = (\[h,m] -> handleTime h m ) $ filter (not . null) $ splitWhen (not . isNumber) time
