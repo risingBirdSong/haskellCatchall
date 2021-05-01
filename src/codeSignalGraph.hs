49fdaaeae5cf385ea6c1bfdec2435eedb05e82dd
peter <peter@Susans-MacBook-Air-2.local>
2021-05-01 15:37:01 -0700

    solved roadsBuilding Graph challenge with the help of Set.difference

--- src/codeSignalGraph.hs
+++ src/codeSignalGraph.hs
@@ -1,4 +1,5 @@
 import Data.List 
+import qualified Data.Set as S
 
 -- https://app.codesignal.com/arcade/graphs-arcade/kingdom-roads/nCMisf4ZKpDLdHevE
 
@@ -13,3 +14,10 @@ mtrxA =[[False, True,  False, False],
 allTrues boolList = filter (\x -> x == True) boolList
 
 newRoadSystem mtrx = all (\b -> b == True) $ zipWith (\ a b -> allTrues a == allTrues b ) mtrx (transpose mtrx)
+
+
+roadsBuilding cities roads = S.toList $ S.difference ( S.fromList [ sort [x,y]  | x <- [0..cities-1] , y <- [0..cities-1], x /= y ] ) (roadSet)
+    where roadSet = S.fromList (doubleSort roads)
+
+doubleSort xxs = sort $ map sort xxs
+
